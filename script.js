79887198c041493ab040fd8c5b6435a2983331a1
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-01-19 10:28:27 +0100

    Dom Manipulation Implemented

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //Create Global Variables for DOM manipulation
-const button_rock = document.querySelector('.rock');
-const button_paper = document.querySelector('.paper');
-const button_scissors = document.querySelector('.scissors');
+const button_rock = document.querySelector('#rock');
+const button_paper = document.querySelector('#paper');
+const button_scissors = document.querySelector('#scissors');
 const buttons = document.querySelectorAll('.choice');
 const reset_button = document.querySelector('#reset')
 const para_score = document.querySelector('#update_score');
 const para_winner = document.querySelector('#determine_winner');
-let playerSelection;
-let computerSelection;
+const para_fullscore = document.querySelector('#full_score');
+para_score.textContent = "-- Each Round Outcome Displayed here --"
+para_fullscore.textContent = "-- Scores Displayed Here --"
+para_winner.textContent = "-- Winner Displayed here --";
+reset_button.style.display = 'none';
 
-
-
-
+let playerScore = 0;
+let computerScore = 0;
 
 // Create a function that will randomly return "Rock", "Paper" or "Scissor"
 // This function is used for the computer's game turn
@@ -19,112 +21,87 @@ function getComputerChoice() {
   let options = ["rock", "paper", "scissors"];
   let computerSelection = options[Math.floor(Math.random() * options.length)];
   return computerSelection;
-}
+};
 
-
-// Create a function that plays a single round and declare winning/losing/tieing scenarios
-
-function playRound() {
-  //get player choice
-  buttons.forEach((button) => {
-    if (button.className = 'choice rock') {
-      playerSelection = 'rock';
-    }
-    else if (button.className ='choice paper') {
-      playerSelection = 'paper';
-    }
-    else if (button.className = 'choice scissors') {
-      playerSelection = 'scissors'
-    }
-  })
-
-  //Winning Scenarios
+//function for game logic and return winner/loser or draw
+function playRound(p, c) {
+  //Winning Scenario
   if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
+    (p === "rock" && c === "scissors") ||
+    (p === "scissors" && c === "paper") ||
+    (p === "paper" && c === "rock")
   ) {
-    para_score.textContent= `You Win! ${playerSelection} Beats ${computerSelection}`;
+    para_score.textContent = `You Win! ${p} Beats ${c}`
+    return "player";
   }
   //Tie Scenario
-  else if (playerSelection === computerSelection) {
-    para_score.textContent= `It is a Tie! NO WINNERS, you both picked ${playerSelection}`;
+  else if (p === c) {
+    para_score.textContent = `Its a Draw you Both Picked ${p} (No Points)`
+    return "draw";
   }
   //Losing Scenarios
   else {
-    para_score.textContent= `You Lose! ${computerSelection} Beats ${playerSelection} `;
+    para_score.textContent = `You Lose! ${c} Beats  ${p}`;
+    return "computer";
   }
 }
 
-computerSelection = getComputerChoice();
-button_rock.addEventListener('click', playRound);
-button_paper.addEventListener('click',playRound);
-button_scissors.addEventListener('click', playRound);
-
-
-
-// create a function that gives 1 point for each round 1 by P or C
-let playerScore = 0;
-let computerScore = 0;
-function updateScores(roundResult) {
-  if (roundResult.includes("You Win!")) {
+function updateScores(winner) {
+  if (winner === "player") {
     playerScore++;
-  } else if (roundResult.includes("You Lose!")) {
+  } else if (winner === "computer") {
     computerScore++;
   }
+  para_fullscore.textContent = `
+  Player Score -- ${playerScore} --
+  Computer Score -- ${computerScore} --`;
+
+  /*add if statement regarding reaching a score of 5 whoever reaches first declare winner
+then remove addeventlistener */
+if (playerScore === 5) {
+  para_winner.textContent = 'Player Wins!'
+  reset_button.style.display ='';
+  buttons.forEach((button) => {
+    button.removeEventListener('click', fullGame)
+  })
+}
+else if (computerScore === 5) {
+  para_winner.textContent = 'Computer Wins!'
+  reset_button.style.display ='';
+  buttons.forEach((button) => {
+    button.removeEventListener('click', fullGame)
+  })
+}
 }
 
+function resetGame () {
+  para_score.textContent = "-- Each Round Outcome Displayed here --";
+  para_fullscore.textContent = "-- Scores Displayed Here --";
+  para_winner.textContent = "-- Winner Displayed here --";
+  playerScore =0;
+  computerScore = 0;
+  reset_button.style.display = 'none'
+  buttons.forEach((button) => {
+    button.addEventListener('click', fullGame)
+  })
+}
 
-
-
-
-
-
-
-// function game() {
-//   for loop to play a best out of 3
-//   for (let round = 1; round <= 10; round++) {
-//     if (playerScore === 3 || computerScore === 3) {
-//       break;
-//     };
-//     let computerSelection = getComputerChoice();
+function fullGame(e) { 
+    const playerSelection = e.currentTarget.id; 
+    const computerSelection = getComputerChoice(); 
+    const winner = playRound(playerSelection,computerSelection);
+    updateScores(winner);  
     
+};
 
-//     let roundResult = playRound(playerSelection,computerSelection);
-//     updateScores(roundResult);
-//     console.log(roundResult);
+buttons.forEach((button) => {
+  button.addEventListener('click', fullGame)
+})
+
+reset_button.addEventListener('click', resetGame)
 
 
 
 
-  
-    
-    
- 
-//    console.log(`-- Round ${round} --`);
-//     let playerSelection = getPlayerChoice();
-    
-//     let roundResult = playRound(playerSelection, computerSelection);
-//     updateScores(roundResult);
-//     console.log(roundResult);
-   
-//     console.log(
-//       `Player Score: ${playerScore} & Computer Score: ${computerScore} `
-//     ); 
-   
-//     }
 
-//   }
 
-// function determineWinner() {
-//   console.log(
-//     `Final Score: Player Score ${playerScore} & Computer Score ${computerScore}`
-//   );
-//   if (playerScore > computerScore) {
-//     console.log("Player Wins! ");
-//   } else if (playerScore < computerScore) {
-//     console.log("Computer Wins!");
-//   }
-// }
-// game();
-// determineWinner();
